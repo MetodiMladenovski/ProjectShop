b8782dd834f983312e105731b90709031a7df702
Metodi Mladenovski <meto.18@outlook.com>
2022-02-22 18:56:51 +0100

    Database dependencies and application properties added

--- src/main/java/com/example/projectshop/model/ShoppingCart.java
+++ src/main/java/com/example/projectshop/model/ShoppingCart.java
@@ -1,6 +1,7 @@
 package com.example.projectshop.model;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ShoppingCart {
@@ -8,8 +9,47 @@ public class ShoppingCart {
     private long id;
     private LocalDateTime dateCreated;
     private User user;
-    List<Device> deviceList;
+
+    private List<Device> deviceList;
 
     public ShoppingCart() {
+    }
+
+    public ShoppingCart(LocalDateTime dateCreated, User user) {
+        this.dateCreated = dateCreated;
+        this.user = user;
+        this.deviceList = new ArrayList<>();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDateTime dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<Device> getDeviceList() {
+        return deviceList;
+    }
+
+    public void setDeviceList(List<Device> deviceList) {
+        this.deviceList = deviceList;
     }
 }
--- src/main/java/com/example/projectshop/model/User.java
+++ src/main/java/com/example/projectshop/model/User.java
@@ -1,12 +1,54 @@
 package com.example.projectshop.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
 
     private String username;
     private String password;
     private String email;
-    private ShoppingCart userCart;
+    private List<ShoppingCart> userCarts;
+
     public User() {
     }
 
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.userCarts = new ArrayList<>();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<ShoppingCart> getUserCart() {
+        return userCarts;
+    }
+
+    public void setUserCart(List<ShoppingCart> userCart) {
+        this.userCarts = userCart;
+    }
 }
