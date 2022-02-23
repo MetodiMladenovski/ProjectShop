2f5146b72ea7672ab148904844272ea8e474c9f8
Mihail <mihailkrzalovski19@gmail.com>
2022-02-23 17:56:18 +0100

    Added Category Service

--- src/main/java/com/example/projectshop/repository/DeviceRepository.java
+++ src/main/java/com/example/projectshop/repository/DeviceRepository.java
@@ -2,9 +2,10 @@ package com.example.projectshop.repository;
 
 import com.example.projectshop.model.Device;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface DeviceRepository extends JpaRepository<Device,Long> {
     Optional<Device>  findByName(String name);
     void deleteByName(String name);
