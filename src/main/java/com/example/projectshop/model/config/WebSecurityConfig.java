eb675fe4505216fe583732dfb580414c273c6cf3
Metodi Mladenovski <meto.18@outlook.com>
2022-04-12 17:03:11 +0200

    Login and Register pages reconfigured, logout button added

--- src/main/java/com/example/projectshop/model/config/WebSecurityConfig.java
+++ src/main/java/com/example/projectshop/model/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -22,24 +23,23 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
-//        http.csrf().disable()
-//                .authorizeRequests()
-//                .antMatchers("/", "/home", "/css/").permitAll()
-//                .anyRequest()
-//                .authenticated()
-//                .and()
-//                .formLogin()
-//                .loginPage("/login").permitAll()
-//                .failureUrl("/login?error=BadCredentials")
-//                .defaultSuccessUrl("/home", true)
-//                .and()
-//                .logout()
-//                .logoutUrl("/logout")
-//                .clearAuthentication(true)
-//                .invalidateHttpSession(true);
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/", "/home", "/register").permitAll()
+                .antMatchers("/resources/static/**").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login").permitAll()
+                .failureUrl("/login?error=BadCredentials")
+                .defaultSuccessUrl("/home", true)
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .clearAuthentication(true)
+                .invalidateHttpSession(true);
     }
-
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) {
